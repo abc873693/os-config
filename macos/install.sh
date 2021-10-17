4d9f861c83289861fa8a77998e7313f6ce59c0e4
Rainvisitor <abc873693@gmail.com>
2021-10-17 16:36:15 +0800

    Add macOS config

---
+++ macos/install.sh
@@ -0,0 +1,25 @@
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+#zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#font-hack-nerd-font
+brew tap caskroom/fonts
+brew install --cask font-hack-nerd-font
+
+#Dozer
+brew install --cask dozer
+
+#Cocoapods
+sudo gem install cocoapods
+
+#Flutter FVM
+brew tap leoafarias/fvm
+brew install fvm
